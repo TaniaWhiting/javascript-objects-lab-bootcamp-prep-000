01a059bb9cf1dd84e3954f08ce578720172197ef
Tania Whiting <tanwhiting@aol.com>
2018-08-16 14:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  ;
+  var clone = Object,assign({}, object);
+  destructivelyUpdateObjectWithKeyAndValue(clone,key);
 }
